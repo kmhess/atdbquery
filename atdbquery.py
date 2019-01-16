895db3c6c9fc3027617b3a188b2c2b360031b9ec
Vanessa Moss <moss@MacMoss.nfra.nl>
2019-01-16 16:59:01 +0100

    Add initial files

---
+++ atdbquery.py
@@ -0,0 +1,51 @@
+# ATDBquery: query ATDB for survey status (ARTS or imaging)
+# V.A. Moss 16/01/2019 (vmoss.astro@gmail.com)
+
+__author__ = "V.A. Moss"
+__date__ = "$16-jan-2019 17:00:00$"
+__version__ = "0.1"
+
+import os
+import sys
+from argparse import ArgumentParser, RawTextHelpFormatter
+from modules.functions import *
+import time
+
+# Function version
+def atdbquery(obs_mode):
+	"""
+    The main program to be run.
+    :return:
+    """
+
+    # Time the total process length
+	start = time.time()
+
+	# Send the query
+	obs_dict = query_database(obs_mode)
+	print('Total number of results returned for %s: %s' % (obs_mode.upper(),len(obs_dict)))
+	print('Unique task IDs: %s' % len(list(set(obs_dict.keys()))))
+
+	# End timing
+	end = time.time()
+	total = end-start
+	print('Total time to run query: %.2f sec' % (total))
+
+	return obs_dict
+
+
+# System call version
+if __name__ == '__main__':
+
+	# Parse the relevant arguments
+	parser = ArgumentParser(formatter_class=RawTextHelpFormatter)
+	parser.add_argument('-m', '--mode',
+			default='sc4',
+			help='Specify whether mode is imaging/sc1/sc4 (default: %(default)s)')
+
+	# Parse the arguments above
+	args = parser.parse_args()
+
+	# Send the query
+	obs_dict = atdbquery(args.mode)
+	print(obs_dict)
---
+++ modules/functions.py
@@ -0,0 +1,66 @@
+# ATDBquery: modules to query ATDB for survey status (ARTS or imaging)
+# V.A. Moss 16/01/2019 (vmoss.astro@gmail.com)
+
+__author__ = "V.A. Moss"
+__date__ = "$16-jan-2019 17:00:00$"
+__version__ = "0.1"
+
+import os
+import sys
+import requests
+import json
+
+###################################################################
+# Query the ATDB database
+
+def query_database(obs_mode):
+
+	# Define the URL for quert
+	url = 'http://atdb.astron.nl/atdb/observations/?my_status__in=completed,completing,archived,removed&observing_mode__icontains=%s' % obs_mode
+
+	# First, determine how many results there are
+	# Do the query
+	try: 
+		response = requests.get(url)
+	except Exception as e:
+		print(e)
+		sys.exit()	
+
+	# Can only do 100 per page
+	result_num = json.loads(response.text)['count']
+	print('Total number of results found in ATDB for %s: %s' % (obs_mode.upper(),result_num))
+	pagenum = result_num // 100 + 1
+
+	# Define the observation dictionary
+	# Get only the field_name,field_ra,field_dec,status
+	obs_dict = {}
+
+	for page in range(1,pagenum+1):
+
+		url = 'http://atdb.astron.nl/atdb/observations/?my_status__in=completed,completing,archived,removed&observing_mode__icontains=%s&page=%s' % (obs_mode,page)
+
+		# Do the query
+		try: 
+			response = requests.get(url)
+		except Exception as e:
+			print(e)
+			sys.exit()
+
+		# Parse the data
+		metadata = json.loads(response.text)['results']
+
+		# for i in range(0,len(metadata)):
+		# 	obs_dict = {**obs_dict,**metadata[i]}
+
+		# Loop
+		for i in range(0,len(metadata)):
+			obs = metadata[i]
+			name = obs['field_name']
+			tid = obs['taskID']
+			ra = obs['field_ra']
+			dec = obs['field_dec']
+			status = obs['my_status']
+
+			obs_dict[tid] = {'field_name':name, 'field_ra':ra, 'field_dec':dec, 'status':status}
+
+	return obs_dict
